050d7a72f2af535b48b1d4fc4acce380d4b9176a
ChiaChi Tsai <rueshyna@gmail.com>
2024-02-08 16:27:46 +0800

    update data

--- ui/src/component/Chart.tsx
+++ ui/src/component/Chart.tsx
@@ -12,7 +12,7 @@ import dataStatisticsThresholdPercentageSans1Of1 from '../data/statisticsThresho
 import dataStatisticsThresholdPercentageWith1OfM from '../data/statisticsThresholdPercentageWith1OfM.json';
 import React from 'react';
 
-const versions = [ "0.0.6", "0.0.8", "0.0.9", "0.0.10", "0.0.11", "0.0.11b", "0.1.1", "0.3.0", "0.3.1", "0.3.2", "0.3.3" ]
+const versions = [ "0.0.6", "0.0.8", "0.0.9", "0.0.10", "0.0.11", "0.0.11b", "0.1.1", "0.3.0", "0.3.1", "0.3.2", "0.3.3", "0.3.4" ]
 const percentage = Array.from(new Set([
   ...Object.keys(dataStatisticsThresholdPercentage),
   ...Object.keys(dataStatisticsThresholdPercentageSans1Of1),
